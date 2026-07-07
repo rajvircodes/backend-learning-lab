e177f7533fd467919931cb892b738ee1636f3be6
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-07-07 22:15:10 +0530

    feat(express-core): implement basic routing and request handling

--- 02-express-core/src/app.js
+++ 02-express-core/src/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Welcome to backend learning lab 😍");
+});
+app.get("/about", (req, res) => {
+  res.send("Welcome to about page");
+});
+app.get("/contact", (req, res) => {
+  res.send("Welcome to contact page");
+});
+app.get("/greet", (req, res) => {
+  const username = req.query.name;
+
+  res.json({
+    message: `Hello ${username}`,
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "Server is healthy!",
+  });
+});
 module.exports = app;
-
-
